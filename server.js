62c10fe37c4a6bdd74bc16f9b60430609316ebe9
shivansh <shivansh4858@gmail.com>
2025-04-14 11:35:06 +0530

    cors

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ const app = express();
 
 
 app.use(express.urlencoded({ extended: true }));
-app.use(cors({ origin: "http://localhost:5173", credentials: true }));  // ✅ Allow cookies in frontend
+app.use(cors({ origin: "https://skill-mate-frontend.vercel.app", credentials: true }));  // ✅ Allow cookies in frontend
 
 app.use(express.json());
 app.use(morgan('dev'));
